6674f2d6922230c705589be9d20fd8ae55a2c52f
Colleen <ottomanc@oregonstate.edu>
2019-04-14 15:20:54 -0400

    add bug adventurerEffect, increase treasuredrawn to 5

--- projects/ottomanc/dominion/dominion.c
+++ projects/ottomanc/dominion/dominion.c
@@ -650,7 +650,7 @@ int adventurerEffect(struct gameState *state, int drawntreasure, int currentPlay
   int temphand[MAX_HAND];
   int z = 0;// this is the counter for the temp hand
 
-  while(drawntreasure<2){
+  while(drawntreasure<5){
     if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
       shuffle(currentPlayer, state);
     }
