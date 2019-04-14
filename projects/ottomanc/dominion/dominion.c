d16cf7178daaf083261c92a3bee54c8fe7a50df1
Colleen <ottomanc@oregonstate.edu>
2019-04-14 15:38:15 -0400

    add bug villageEffect, remove the increase actions

--- projects/ottomanc/dominion/dominion.c
+++ projects/ottomanc/dominion/dominion.c
@@ -723,9 +723,6 @@ int villageEffect(struct gameState *state, int currentPlayer, int handPos)
   //+1 Card
   drawCard(currentPlayer, state);
 			
-  //+2 Actions
-  state->numActions = state->numActions + 2;
-			
   //discard played card from hand
   discardCard(handPos, currentPlayer, state, 0);
   return 0;
