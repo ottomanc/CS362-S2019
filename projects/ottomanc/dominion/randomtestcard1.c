e155aca9146485bc92e391d95a9cf8baa86e25c5
Colleen <ottomanc@oregonstate.edu>
2019-05-20 01:04:52 -0400

    add code to random testcard1

--- projects/ottomanc/dominion/randomtestcard1.c
+++ projects/ottomanc/dominion/randomtestcard1.c
@@ -13,22 +13,18 @@
 #define DEBUG 0
 #define NOISY_TEST 1
 
-int checkCouncilRoom(int thisPlayer, struct gameState *post) {
-
+int checkCouncilRoom(int p, struct gameState *post){
+  struct gameState pre;
+  memcpy (&pre, post, sizeof(struct gameState));
   int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
 
-  struct gameState pre;
-
   int r;
-
-  memcpy(&pre, post, sizeof(struct gameState));
-
-  //run the cardEffect function with the random gameState setup
+ 
   r = cardEffect(adventurer, choice1, choice2, choice3, post, handpos, &bonus);
 
   assert (r == 0);
-  assert(memcmp(&pre, post, sizeof(struct gameState)) == 0);
 
+  assert(memcmp(&pre, post, sizeof(struct gameState)) == 0);
 }
 
 int main () {
