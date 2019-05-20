b785754086dad4f09343bb733ddf205879f878d3
Colleen <ottomanc@oregonstate.edu>
2019-05-20 00:47:26 -0400

    changed assert statements

--- projects/ottomanc/dominion/randomtestadventurer.c
+++ projects/ottomanc/dominion/randomtestadventurer.c
@@ -13,7 +13,7 @@
 #define DEBUG 0
 #define NOISY_TEST 1
 
-int checkAdventurer(int thisPlayer, struct gameState *G) {
+int checkAdventurer(int thisPlayer, struct gameState *post) {
   int newCards = 0;
   int discarded = 1;
   //int xtraCoins = 0;
@@ -22,23 +22,28 @@ int checkAdventurer(int thisPlayer, struct gameState *G) {
   //int i;
   int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
 
-  struct gameState testG;
+  struct gameState pre;
+
+  int r;
 
 	// copy the game state to a test case
-	memcpy(&testG, G, sizeof(struct gameState));
+	memcpy(&pre, post, sizeof(struct gameState));
 
   //run the cardEffect function with the random gameState setup
-	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
+	r = cardEffect(adventurer, choice1, choice2, choice3, post, handpos, bonus);
 
   //Check the results with assert statements
-	newCards = 2;
-	printf("hand count = %d, expected = %d\n", testG.handCount[thisPlayer], G->handCount[thisPlayer] + newCards - discarded);
-  printf("deck count = %d, expected = %d\n", testG.deckCount[thisPlayer], G->deckCount[thisPlayer] - newCards + shuffledCards);
+	//newCards = 2;
+	//printf("hand count = %d, expected = %d\n", post->handCount[thisPlayer], pre.handCount[thisPlayer] + newCards - discarded);
+  //printf("deck count = %d, expected = %d\n", post->deckCount[thisPlayer], pre.deckCount[thisPlayer] - newCards + shuffledCards);
+  //check if the function happened
+  assert (r == 0);
+  assert(memcmp(&pre, post, sizeof(struct gameState)) == 0);
   //Check the hand count
-	assert(testG.handCount[thisPlayer] == G->handCount[thisPlayer] + newCards - discarded);
+	//assert(post->handCount[thisPlayer] == pre.handCount[thisPlayer] + newCards - discarded);
   //Check the deck count
-	assert(testG.deckCount[thisPlayer] == G->deckCount[thisPlayer] - newCards + shuffledCards);
-//return 0;
+	//assert(post->deckCount[thisPlayer] == pre.deckCount[thisPlayer] - newCards + shuffledCards);
+  //return 0;
 }
 
 int main () {
