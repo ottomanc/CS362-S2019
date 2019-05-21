68e19277f3915cdb7e64434866d43f1f1d34d4e2
Colleen <ottomanc@oregonstate.edu>
2019-05-21 12:35:56 -0400

    added a lot to the game intialization

--- projects/ottomanc/dominion/randomtestcard1.c
+++ projects/ottomanc/dominion/randomtestcard1.c
@@ -30,8 +30,9 @@ int checkCouncilRoom(int p, struct gameState *post){
 
 int main () {
 
-  int i, n, p;
-
+  int i, j, n, p, numPlayers, it;
+  //int numPlayers = 2;
+  //int seed = 1000;
   struct gameState G;
 
   printf ("Testing Council Room Card.\n");
@@ -40,22 +41,60 @@ int main () {
 
   SelectStream(2);
   PutSeed(3);
+  int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+		sea_hag, tribute, smithy, council_room};
 
   for (n = 0; n < 2000; n++) {
     for (i = 0; i < sizeof(struct gameState); i++) {
       ((char*)&G)[i] = floor(Random() * 256);
     }
-    p = floor(Random() * 2);
-    G.whoseTurn = p;
+    p = floor(Random() * numPlayers);
+    //G.whoseTurn = p;
     G.playedCardCount = floor(Random() * MAX_HAND);
-    G.deckCount[p] = floor(Random() * MAX_DECK);
+    //G.deckCount[p] = floor(Random() * MAX_DECK);
     G.discardCount[p] = floor(Random() * MAX_DECK);
     G.handCount[p] = floor(Random() * MAX_HAND);
 
-    //set up the card piles
-    memset(G.deck[p], 0, sizeof(int) * G.deckCount[p]);
-	memset(G.discard[p], 0, sizeof(int) * G.discardCount[p]);
-	memset(G.hand[p], 0, sizeof(int) * G.handCount[p]);
+    //set player decks
+    for (i = 0; i < numPlayers; i++)
+    {
+      G.deckCount[i] = 0;
+      for (j = 0; j < 3; j++)
+        {
+        G.deck[i][j] = estate;
+        G.deckCount[i]++;
+        }
+      for (j = 3; j < 10; j++)
+        {
+        G.deck[i][j] = copper;
+        G.deckCount[i]++;		
+        }
+    }
+
+
+    //create player hands
+    for (i = 0; i < numPlayers; i++)
+    {  
+        //initialize hand size to Random
+        G.handCount[i] = floor(Random() * MAX_DECK);
+        G.discardCount[i] = floor(Random() * MAX_DECK);
+    }
+
+    //initialize first player's turn
+    G.outpostPlayed = 0;
+    G.phase = 0;
+    G.numActions = 1;
+    G.numBuys = 1;
+    G.playedCardCount = 0;
+    G.whoseTurn = p;
+    G.handCount[G.whoseTurn] = 0;
+
+    //Moved draw cards to here, only drawing at the start of a turn
+    for (it = 0; it < G.handCount[p]; it++){
+        drawCard(G.whoseTurn, &G);
+    }
+
+    updateCoins(G.whoseTurn, &G, 0);
 
 
     printf("now going to check council room");
