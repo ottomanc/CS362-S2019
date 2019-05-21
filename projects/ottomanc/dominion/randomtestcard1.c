7153e4213d3d46b23a5a1188676a2bd528b95daa
Colleen <ottomanc@oregonstate.edu>
2019-05-21 15:28:41 -0400

    add print statements &

--- projects/ottomanc/dominion/randomtestcard1.c
+++ projects/ottomanc/dominion/randomtestcard1.c
@@ -41,7 +41,7 @@ int checkCouncilRoom(int p, struct gameState *post){
   pre.numBuys++;
 
   assert(pre.numBuys == post->numBuys);
-  printf("pre handcount: %d; post handcount: %d\n", pre.handCount, post->handCount);
+  printf("pre handcount: %d; post handcount: %d\n", &pre.handCount, &post->handCount);
   assert(pre.handCount == post->handCount);
   assert(pre.discardCount == post->discardCount);
 
